56cab2f195e4e3adc8e1459ec0b9e341baee91e3
lihua01 <363088847@qq.com>
2019-09-20 17:53:00 +0800

    第二次提交
    Python

---
+++ bot.py
@@ -0,0 +1,11 @@
+a=[0,10,'11','hello','zhangsan']
+if "11" in a:
+    print ("11 in the list")
+
+if "11" not in a:
+    print ("11 in not in the list")
+
+dict_1 = {'student_id':1001,'sex':21}
+if dict_1['student_id'] == 1001:
+    if dict_1['sex'] >20:
+        print("x学号为1001.且年龄大于20")
---
+++ greeter.py
@@ -0,0 +1,31 @@
+#向函数传递参数
+'''
+def greete_user(username):
+    """显示简单的问候语"""
+    print("Hello, "+ username.title( ) + "!")
+        
+greete_user('王宁')
+
+def display_message():
+	print("我学了Python的函数")
+	
+display_message()
+
+def favorite_book(title):
+	print("One of my favorite books is "+title.title())
+favorite_book('alice wonderland')
+'''
+
+
+	
+	
+
+
+
+
+
+
+
+
+
+
